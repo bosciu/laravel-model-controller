e62600c57b2685dfeb28c1e7ecfae6059467a0f7
bosciu <bosciu@icloud.com>
2021-07-14 16:42:30 +0200

    Aggiunta sezione commentata con wuery where

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,11 @@ use App\Movie;
 class HomeController extends Controller
 {
     public function index(){
+        //Per poter fare una query e filtrare i dati si può fare anche una variabile a parte
+        /* $query = Movie::where('title', 'LIKE', 'I%');
+        @dump($query->toSql());
+        @dump($query->getBindings());
+        $movies = $query->get(); */
         $movies = Movie::all();
         return view('homepage', compact('movies'));
     }
